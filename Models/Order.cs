7c3449e012eea62fe034352140399e37c2b0f164
yadian <yadianfdez07@gmail.com>
2021-09-06 19:53:37 -0400

    setup project

---
+++ Models/Order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public List<Item> Items { get; set; }
+    }
+}
